2512054deeaffbc7a75426305f50c4d49642ff59
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-27 14:46:07 +0900

    to_hex実装
    to_hexメソッド実装

---
+++ test/rgb_tesst.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require_relative '../lib/rgb'
+
+class RGBTest < Minitest::Test
+    def test_to_hex
+        assert_equal '#000000', to_hex(0, 0, 0)
+        assert_equal '#ffffff', to_hex(255, 255, 255)
+    end
+end
