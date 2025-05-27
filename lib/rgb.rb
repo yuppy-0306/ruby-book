c8af9f07c7df242a7af72473ed27edbaf5ef54df
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-27 15:17:37 +0900

    to_intsメソッド実装
    to_intsメソッド実装

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,6 +1,16 @@
 def to_hex(r, g, b)
-    '#' + 
-    r.to_s(16).rjust(2, '0') +
-    g.to_s(16).rjust(2, '0') +
-    b.to_s(16).rjust(2, '0')
+  [r, g, b].sum('#') do |n|
+    n.to_s(16).rjust(2, '0')
+  end
+end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  ints = []
+  [r, g, b].each do |s|
+    ints << s.hex
+  end
+  ints
 end
