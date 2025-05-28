75f2805ebfed7c98c47e27bc0dc147139b91b778
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-28 13:36:00 +0900

    例外処理追加

--- lib/regexp_checker.rb
+++ lib/regexp_checker.rb
@@ -3,10 +3,18 @@ text = gets.chomp
 print 'Pattern?: '
 pattern = gets.chomp
 
-regexp = Regexp.new(pattern)
+begin
+    print 'Pattern?: '
+    pattern = gets.chomp
+    regexp = Regexp.new(pattern)
+rescue RegexpError => e
+    puts "Invalid pattern: #{e.message}"
+    retry
+end
+
 matches = text.scan(regexp)
 if matches.size > 0
   puts "Matched: #{matches.join(', ')}"
 else
-  puts 'Nothng matched.'
+  puts 'Nothing matched.'
 end
