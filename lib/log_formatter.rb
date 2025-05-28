5b4323529970ab833797b059dff260f0d68c2dd8
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-28 15:33:00 +0900

    "OK"ログ対応

--- lib/log_formatter.rb
+++ lib/log_formatter.rb
@@ -7,6 +7,12 @@ module LogFormatter
         uri = URI.parse('https://samples.jnito.com/access-log.json')
         json = Net::HTTP.get(uri)
         log_data = JSON.parse(json,symbolize_names: true)
-        pp log_data
+        
+        log_data.map do |log|
+            case log
+            in {request_id:, path:}
+              "[OK] request_id=#{request_id}, path=#{path}"
+        end
+        end.join("\n")
     end
 end
--- test/log_formatter_test.rb
+++ test/log_formatter_test.rb
@@ -3,6 +3,8 @@ require_relative '../lib/log_formatter'
 
 class LogFormatterTest < Minitest::Test
     def test_format_log
-      LogFormatter.format_log
+        text = LogFormatter.format_log
+        lines = text.lines(chomp: true)
+        assert_equal '[OK] request_id=1, path=/products/1', lines[0]
     end
 end
