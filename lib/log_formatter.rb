c386e1d6747eda4cfa23c113dfea3cbda1f3b287
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-28 15:36:28 +0900

    404エラーログ対応

--- lib/log_formatter.rb
+++ lib/log_formatter.rb
@@ -10,6 +10,8 @@ module LogFormatter
         
         log_data.map do |log|
             case log
+            in { request_id:, path:, status: 404, error: }
+                "[ERROR] request_id=#{request_id}, path=#{path}, status=404, error=#{error}"
             in {request_id:, path:}
               "[OK] request_id=#{request_id}, path=#{path}"
         end
--- test/log_formatter_test.rb
+++ test/log_formatter_test.rb
@@ -6,5 +6,6 @@ class LogFormatterTest < Minitest::Test
         text = LogFormatter.format_log
         lines = text.lines(chomp: true)
         assert_equal '[OK] request_id=1, path=/products/1', lines[0]
+        assert_equal '[ERROR] request_id=2, path=/wp-login.php, status=404, error=Not found', lines[1]
     end
 end
