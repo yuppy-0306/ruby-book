45410974ec3f6326bb04d107330031343e5c5e1c
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-28 13:17:03 +0900

    rainbowメソッドリファクタリング

--- lib/rainbowable.rb
+++ lib/rainbowable.rb
@@ -1,14 +1,8 @@
 module Rainbowable
   def rainbow
-    str = self.to_s
-    count = 0
-    colored_chars = []
-    str.each_char do |char|
+    to_s.each_char.map.with_index do |char, count|
         color = 31 + (count % 6)
-        colored_chars << "\e[#{color}m#{char}"
-        count += 1
-    end
-    ret = colored_chars.join
-    ret + "\e[0m"
+        "\e[#{color}m#{char}"
+    end.join + "\e[0m"
   end
 end
