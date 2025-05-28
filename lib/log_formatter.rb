908068545a3f7832d4fcabf6ce24765ad554307a
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-28 15:45:10 +0900

    500エラー対応

--- lib/log_formatter.rb
+++ lib/log_formatter.rb
@@ -3,20 +3,20 @@ require 'uri'
 require 'json'
 
 module LogFormatter
-    def self.format_log
-        uri = URI.parse('https://samples.jnito.com/access-log.json')
-        json = Net::HTTP.get(uri)
-        log_data = JSON.parse(json,symbolize_names: true)
-        
-        log_data.map do |log|
-            case log
-            in { request_id:, path:, status: 404, error: }
-                "[ERROR] request_id=#{request_id}, path=#{path}, status=404, error=#{error}"
-            in {request_id:, path:, duration: 1000.. => duration}
-                "[WARN] request_id=#{request_id}, path=#{path}, duration=#{duration}"
-            in {request_id:, path:}
-                "[OK] request_id=#{request_id}, path=#{path}"
-        end
-        end.join("\n")
-    end
+  def self.format_log
+    uri = URI.parse('https://samples.jnito.com/access-log.json')
+    json = Net::HTTP.get(uri)
+    log_data = JSON.parse(json, symbolize_names: true)
+
+    log_data.map do |log|
+      case log
+      in {request_id:, path:, status: 404 | 500 => status, error:}
+        "[ERROR] request_id=#{request_id}, path=#{path}, status=#{status}, error=#{error}"
+      in {request_id:, path:, duration: 1000.. => duration}
+        "[WARN] request_id=#{request_id}, path=#{path}, duration=#{duration}"
+      in {request_id:, path:}
+        "[OK] request_id=#{request_id}, path=#{path}"
+      end
+    end.join("\n")
+  end
 end
