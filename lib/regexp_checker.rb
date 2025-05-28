51733389bcc39a96655bc74bbcb1106d1a236ef1
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-28 13:33:09 +0900

    getsメソッド実装

---
+++ lib/regexp_checker.rb
@@ -0,0 +1,12 @@
+print 'Text?: '
+text = gets.chomp
+print 'Pattern?: '
+pattern = gets.chomp
+
+regexp = Regexp.new(pattern)
+matches = text.scan(regexp)
+if matches.size > 0
+  puts "Matched: #{matches.join(', ')}"
+else
+  puts 'Nothng matched.'
+end
