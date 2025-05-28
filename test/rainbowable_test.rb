19e4fdc54feadd772f33c883c91b41dbdb94477a
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-05-28 13:19:57 +0900

    rainbowメソッド出力

--- test/rainbowable_test.rb
+++ test/rainbowable_test.rb
@@ -3,8 +3,7 @@ require_relative '../lib/rainbowable'
 
 class RainbowableTest < Minitest::Test
   def setup
-    String.include Rainbowable
-    Array.include Rainbowable
+    Object.include Rainbowable
   end
 
   def test_rainbow
@@ -13,5 +12,9 @@ class RainbowableTest < Minitest::Test
 
     expected = "\e[31m[\e[32m1\e[33m,\e[34m \e[35m2\e[36m,\e[31m \e[32m3\e[33m]\e[0m"
     assert_equal expected, [1, 2, 3].rainbow
+    puts({foo: 123, bar: 456}.rainbow)
+    puts (10..20).rainbow
+    puts true.rainbow
+    puts false.rainbow
   end
 end
